cbd23e834c650943714842a5aee5d593f427645e
andremarinho <andre.bmarinho@hotmail.com>
2020-07-31 00:44:00 -0300

    Init project

---
+++ src/main/java/com/devolucao/DevolucaoApiApplication.java
@@ -0,0 +1,13 @@
+package com.devolucao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevolucaoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevolucaoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devolucao/DevolucaoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devolucao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevolucaoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
